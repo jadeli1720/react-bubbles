5874280fbccfa37215d74b65ee10f374562b0d69
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-23 09:36:02 -0600

    Created login form with change handler and a sumbit event handler. Also adde semantic ui to use for form styling right away

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
+import { Form } from 'semantic-ui-react';
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+    console.log(
+      "handleChange",
+      e.target.name,
+      e.target.value,
+      setLogin()
+    );
+  }
+
+  const loginSubmit = e => {
+    e.preventDefault();
+
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div className="formContainer">
+        <h2>Please Login Below</h2>
+        <Form onSubmit={loginSubmit} className="loginForm">
+          <Form.Field className="loginField">
+            <label className="loginLabel" >Username:</label>
+            <input
+              type="text"
+              name="username"
+              placeholder='username'
+              // fluid
+              className="loginInput"
+              // width={6}//don't know if I'll need
+              value={login.username}
+              onChange={handleChange}
+            />
+          </Form.Field>
+          <Form.Field className="loginField">
+            <label className="loginLabel" >Password:</label>
+            <input
+              type="password"
+              name="password"
+              placeholder='password'
+              // fluid
+              className="loginInput"
+              width={6}
+              value={login.password}
+              onChange={handleChange} />
+          </Form.Field>
+          <button type="submit" className="loginButton">Log in</button>
+
+        </Form>
+      </div>
     </>
   );
 };
