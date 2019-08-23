abcf9e3b1a741fa1675219372cc1dc7e82d75f52
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-23 10:14:37 -0600

    Working on delete functionality. Logged and confirmed that click the button selects the correct color id

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -25,6 +26,13 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    console.log(color);//logging the color of the delete color on button click
+    axiosWithAuth()
+        .delete(`http://localhost:5000/api/colors/${color.id}`)
+        .then(res => {
+          console.log('Delete Color', res.data)
+        })
+        .catch(err => console.log('Opps, could not delete color', err.response));
   };
 
   return (
